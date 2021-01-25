a2506167abed2bbc60b5c8bca27e182053f165ea
max kolomeychenko <maxim.kolomeychenko@gmail.com>
2021-01-25 22:43:22 +0200

    drop event next image

--- src/main.py
+++ src/main.py
@@ -8,35 +8,6 @@ import batches
 import tagging
 
 
-@ag.app.callback("manual_selected_image_changed")
-def event_next_image(api: sly.Api, task_id, context, state, app_logger):
-    user_id = context["userId"]
-    cur_image_id = context["imageId"]
-    if cur_image_id is None or type(cur_image_id) is not int:
-        app_logger.warn("Image Changed event with wrong context", extra={"ui_context": context})
-        return
-
-    cur_image_info = api.image.get_info_by_id(cur_image_id)
-    field = cur_image_info.meta.get(ag.field_name, None)
-
-    if field is None:
-        fields = {
-            "fieldNotFound": "Field {!r} not found".format(ag.field_name),
-            "fieldValue": "",
-            "catalogInfo": {}
-        }
-    else:
-        catalog_info = catalog.index.get(field, None)
-        fields = {
-            "fieldNotFound": "" if catalog_info is not None else "Key {!r} not found in catalog".format(field),
-            "fieldValue": field,
-            "catalogInfo": catalog_info,
-        }
-
-    api.app.set_field(task_id, "data.user", {user_id: fields}, append=True)
-    references.refresh_grid(user_id, field)
-
-
 def main():
     ag.init()
 
